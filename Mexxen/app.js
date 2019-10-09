5b1f48f3bd3894048d2b205d5ac0f0f0b008b74b
Joost Niessen <j.niessen@student.fontys.nl>
2019-10-09 15:03:58 +0200

    First functionality: Number replacer

--- Mexxen/app.js
+++ Mexxen/app.js
@@ -1,4 +1,4 @@
-var score, lastScore, shotCount, res;
+var score, lastScore, shotCount;
 
 shotCount = 1;
 
@@ -13,30 +13,12 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
     score = "" + dice1 + dice2 + "";
 
-
-
     if (dice2 > dice1) {
-        res = score.replace(dice1, dice2) - dice2 + dice1;
+        var res = score.replace(dice1, dice2) - dice2 + dice1;
         document.getElementById("score").innerHTML = res;
-    } else if (dice1 === dice2) {
-        score = dice1 * 100;
-        document.getElementById("score").innerHTML = score;       
-    } else if (score == 21 || res == 21) {
-        shotCount = shotCount + 1;
-        document.getElementById("shot-count").innerHTML = shotCount;   
-        document.getElementById("score").innerHTML = score;       
-    } else if (dice1 > dice2) {
-        document.getElementById("score").innerHTML = score;
+    } else {
+        score = dice1 + dice2;
     }
-
-  
-
-
-
-    
-
-    
-
     
 })
 
