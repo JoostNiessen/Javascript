d2123351621203100d5d3fa86e81027adae4c39c
Joost Niessen <j.niessen@student.fontys.nl>
2019-10-24 11:31:28 +0200

    Rebuilt the code for better understanding

--- Advanced-JS/starter/script.js
+++ Advanced-JS/starter/script.js
@@ -1,42 +1,57 @@
 // var john = {
-//     name: 'John',
+//     name: 'john',
 //     yearOfBirth: 1990,
 //     job: 'teacher'
-// }
+// };
 
 // var Person = function(name, yearOfBirth, job) {
 //     this.name = name;
 //     this.yearOfBirth = yearOfBirth;
 //     this.job = job;
+//     var age;
 // }
-
 
 // Person.prototype.calculateAge = function() {
-//     console.log(2016-this.yearOfBirth);
+//     age = 2016 - this.yearOfBirth;
+//     console.log(age);
 // }
 
-// Person.prototype.lastName = 'Smith';
+// Person.prototype.isElder = function() {
+//     if (age > 50) {
+//         console.log(this.name + ' is Old');      
+//     } else {
+//         console.log(this.name + ' is Young');    
+//     }
+// }
 
-// var john = new Person('John', 1990, 'teacher');
+// Person.prototype.nationality = 'Polish';
 
-// var jane = new Person('Jane', 1969, 'teacher');
+// var henk = new Person('Henk', 1965, 'Streetmaker');
+// var jos = new Person('Jos', 1970, 'IT-specialist');
+// var maria = new Person('Maria', 1980, 'Teacher');
 
-// console.log(jane);
+// maria.calculateAge();
+
+// maria.isElder();
+
+// Primitives vs Objects
 
 var personProto = {
     calculateAge: function() {
-        console.log(2016 - yearOfBirth);
+        console.log(2016 - this.yearOfBirth);      
     }
-};
+}
 
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'teacher';
+var number = 'apenkut';
 
-
-var jane = Object.create(personProto, {
-    name: { value: 'jane'},
-    yearOfBirth: { value: 1969 },
-    job: { value: 'designer'} 
+var monique = Object.create(personProto, {
+    name: { value: 'Monique'},
+    zipCode: { value: '6060QQ' },
+    lastName: { value: 'Fetcher' }
 })
+
+var marcel = Object.create(personProto);
+marcel.name = 'Marcel';
+marcel.zipCode = '5050ZZ';
+marcel.lastName = 'Spencer';
+
