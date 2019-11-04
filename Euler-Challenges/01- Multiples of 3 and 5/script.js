87481ca618c98c49d1eaf6d52bd72f454c65d68b
Joost Niessen <j.niessen@student.fontys.nl>
2019-11-04 10:02:28 +0100

    First commit

---
+++ Euler-Challenges/01- Multiples of 3 and 5/script.js
@@ -0,0 +1,44 @@
+var n;
+var totalNumber;
+var variableNumber;
+
+function divideNumber(totalNumber, variableNumber) {
+    n = Math.floor(totalNumber / variableNumber);
+    console.log("n: " + n);  
+    return n;
+}
+
+function getSum(x){
+    var sum = 0;
+    for (i = 0; i < n; i++) {
+        sum += x;
+        console.log(sum); 
+    }
+}
+
+var three = divideNumber(9, 3);
+
+var sum = getSum(3);
+
+console.log(sumThree);
+
+// console.log(three + five);
+
+
+
+
+
+
+
+// get the multiples below 10 from 5 or 3
+var five = Math.floor(9 / 5);
+var three = Math.floor(9 / 3);
+
+// put these in an array
+for ()
+
+
+// sum up the array
+
+
+
