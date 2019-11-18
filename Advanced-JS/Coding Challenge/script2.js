17a23b5ec7922cb8562d286299214146f9e8b3f5
Joost Niessen <j.niessen@student.fontys.nl>
2019-11-18 15:27:18 +0100

    Updated and finished solution

--- Advanced-JS/Coding Challenge/script2.js
+++ Advanced-JS/Coding Challenge/script2.js
@@ -1,18 +1,9 @@
-// put the question numbers in the () of the function field. Log the question field op the object in the function with the right number 
-// passed in. 
-
-//  return function (question) {
-//     if (iRnd === 0) {
-//         log question.question blabla
-//         add score ++
-//     }
-// }
-
 function Question(question, answers, correct) {
     this.question = question;
     this.answers = answers;
     this.correct = correct;
 }
+
 
 var question1 = new Question('What is the sum of 5 + 5?', [10, 12, 2], 0);
 var question2 = new Question('What is the sun made of?', ['water', 'fire', 'air'], 1);
@@ -28,22 +19,52 @@ Question.prototype.displayQuestion = function() {
     }
 }
 
-
-var n  = Math.floor(Math.random() * questions.length);
-
-questions[n].displayQuestion();
-
-Question.prototype.correctAnswer = function(ans) {
+Question.prototype.correctAnswer = function(ans, callback) {
+    var sc;
     if(ans === this.correct) {
-        console.log("Correct answer!!!");
+        console.log("Correct answer!!!");   
+        sc = callback(true);
     } else {
         console.log("Wrong answer...");
+        sc = callback(false);
+    }
+
+    this.displayScore(sc);
+}
+
+Question.prototype.displayScore = function(score) {
+    console.log('Your current score: ' + score);
+    console.log('---------------------------');
+    
+}
+
+function score() {
+    var sc = 0;
+    return function(correct) {
+        if (correct) {
+            sc++;
+        }
+        return sc;
     }
 }
 
-var answer = parseInt(prompt("Please select the right answer!"));
+var keepScore = score();
 
-questions[n].correctAnswer(answer);
+function nextQuestion() {
+    var n  = Math.floor(Math.random() * questions.length);
 
+    questions[n].displayQuestion();
+    
+    var answer = prompt("Please select the right answer!");
+    
+
+    if (answer !== 'exit') {
+        questions[n].correctAnswer(parseInt(answer), keepScore);
+
+        nextQuestion();
+    }
+}
+
+nextQuestion();
 
 
