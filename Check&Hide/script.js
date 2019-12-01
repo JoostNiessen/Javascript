94d3ccfbd80087ba54ddf22aeb7518fdfeba88c4
Joost Niessen <j.niessen@student.fontys.nl>
2019-12-01 10:20:11 +0100

    First working script

--- Check&Hide/script.js
+++ Check&Hide/script.js
@@ -1,13 +1,33 @@
-var task;
+var taskList = [];
 
+document.getElementById('addBtn').addEventListener('click', function() {
+    var task = document.getElementById('addTask').value;
 
-document.querySelector('.btnAdd').addEventListener('click', function() {
-    task = document.getElementById('addTask').value;
-    console.log('free');
+    taskList.push(task);
+    console.log(taskList);
+
+    for (i = 0; i < taskList.length; i++) {
+        task = taskList[i]
+    }
+    addItemToList(task);      
     
-    document.querySelector('.todo-item').textContent = task;
-
 });
+
+
+
+
+var addItemToList = function(task) {
+    var node = document.createElement("LI"); 
+    node.className = 'list-group-item';                // Create a <li> node
+    var textnode = document.createTextNode(task);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.querySelector(".list1").appendChild(node);     // Append <li> to <ul> with id="myList"
+}
+
+
+
+
+
 
 
 
