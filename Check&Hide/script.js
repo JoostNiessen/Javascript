39b484402461af68cdd8299ffd24b0428deb7d7e
Joost Niessen <j.niessen@student.fontys.nl>
2019-12-02 15:49:11 +0100

    remove selected elements + function comments

--- Check&Hide/script.js
+++ Check&Hide/script.js
@@ -2,19 +2,99 @@ var taskList = {
     tasks: []
 }
 
+
+
+/***
+ * Eventlistener on the + button in the DOM
+ * sets the input field to ''
+ * Contains:
+ * addInputValuesToArray();
+ * getInputValues(); // closure
+ * upDateUI();
+ */
+document.getElementById('addBtn').addEventListener('click', function() {
+    
+    addInputValuesToArray(getInputValues());
+    
+    updateUI();
+     
+    console.log(taskList);
+
+    document.querySelector('#taskInput').value = '';
+    
+});
+
+
+
+/**
+ * Eventlistener on the CLEAR LIST button in the DOM
+ * clears array of checked items
+ * clears the list of elements
+ * Contains:
+ * clearArray();
+ * clearListElements();
+ */
+document.getElementById('clearList').addEventListener('click', function() {
+    clearArray();
+    clearListElements();
+
+    console.log(taskList.tasks);
+    
+});
+
+
+
+/**
+ * create array of all elements with tag <li> inside of #list1
+ * loop through array to find 'checked' elements
+ * if there are 'checked' elements, remove those
+ */
+function clearListElements() {
+    var ul = document.getElementById("list1");
+    var items = ul.getElementsByTagName("li");
+    console.log(items.length);
+    
+    for (var i = 0; i < items.length + 1; ++i) {
+        var el = document.querySelector('.checked');
+
+        if (el !== null){
+            el.remove(); 
+        }
+    }
+}
+
+
+
+/***
+ * sets the 'title' of the to do list
+ * NOTE: this needs to be dynamic
+ */
 document.querySelector('.header').textContent = 'Monday';
 
+
+/**
+ * gets input from DOM and returns it
+ */
 function getInputValues() {
     var taskName = document.querySelector('#taskInput').value;
     return taskName;
 }
 
+
+/***
+ * requires value to push to array
+ */
 function addInputValuesToArray(taskName) {
     taskList.tasks.push(taskName);
 }
 
+/**
+ * Loop: Gets all tasks from the array
+ *       Creates a <li> element for every task and assigns Bootstrap-class to it
+ * Appends listItems to List 
+ */
 function updateUI() {
-
+    
     for(i = 0; i < taskList.tasks.length; i++) {
 
         var task = taskList.tasks[i];
@@ -29,35 +109,10 @@ function updateUI() {
 }
 
 
-document.getElementById('addBtn').addEventListener('click', function() {
-    
-    addInputValuesToArray(getInputValues());
-    
-    updateUI();
-     
-    console.log(taskList);
-
-    document.querySelector('#taskInput').value = '';
-
-    
-    
-});
-
-
-
-document.getElementById('clearList').addEventListener('click', function() {
-    clearArray();
-
-    var ul = document.getElementById("list1");
-    var items = ul.getElementsByTagName("li");
-    for (var i = 0; i < items.length; ++i) {
-        var el = document.querySelector('.list-group-item');
-        el.remove(); // Removes the div with the 'div-02' id
-    }
-    
-});
-
-
+/**
+ * Selects UL, adds eventlistener on target
+ * if target is LI toggle .checked, specified in the css
+ */
 var list = document.querySelector('.list-group');
     list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
@@ -72,21 +127,11 @@ var list = document.querySelector('.list-group');
 
 
 function clearArray() {
-    for(i = 0; i < taskList.tasks.length + 2; i++) {
-        taskList.tasks.pop(); 
-    } 
+
+    // clear only selected tasks
+        taskList.tasks = [];
 }
 
-
-
-var addItemToList = function(task) {
-     // Append <li> to <ul> with id="myList"
-     var list = document.createElement("LI"); 
-     list.className = 'list-group-item';                // Create a <li> node
-     var listItem = document.createTextNode(task.name);         // Create a text node
-     list.appendChild(listItem);                              // Append the text to <li>
-     document.getElementById("list1").appendChild(list);
-}
 
 
 
