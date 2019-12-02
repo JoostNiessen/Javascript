9b91b4cad061f899e28c70dd0d8d974d672dbe18
Joost Niessen <j.niessen@student.fontys.nl>
2019-12-02 10:33:59 +0100

    created functions for the actions

--- Check&Hide/script.js
+++ Check&Hide/script.js
@@ -7,61 +7,76 @@ var task = {
     priority: 0
 };
 
-// taskList1.tasks.push(task); 
 
-// task.name = 'freddy';
-// task.priority = 9;
+
+// first fill list with object tasks, then load all the tasks in the list with a for loop
+
+function getInputValue() {
+    task.name = document.getElementById('taskInput').value;
+}
+
+function addTaskToArray() {
+    taskList1.tasks.push(task); 
+}
+
+function showListInUI() {
+    for (i = 0; i < taskList1.tasks.length; i++) {
+        task = taskList1.tasks[i];
+        
+    }
+    var list = document.createElement("LI"); 
+    list.className = 'list-group-item';                // Create a <li> node
+    var listItem = document.createTextNode(task.name);         // Create a text node
+    list.appendChild(listItem);                              // Append the text to <li>
+    document.getElementById("list1").appendChild(list);
+
+}
 
 
 document.querySelector('.header').textContent = taskList1.listName;
 
 
 document.getElementById('addBtn').addEventListener('click', function() {
-    var task = document.getElementById('taskInput').value;
+    getInputValue();
 
-    taskList1.tasks.push(task);
-    console.log(taskList1.tasks);
+    addTaskToArray();
 
-    for (i = 0; i < taskList1.tasks.length; i++) {
-        task = taskList1.tasks[i]
-    }
+    showListInUI();
+
+
     addItemToList(task);    
     
+    // set empty input field
     document.getElementById('taskInput').value = '';
+
+    console.log(taskList1.tasks);
+    
     
 });
 
 
-
 var list = document.querySelector('.list-group');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
+    list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+
     ev.target.classList.toggle('checked');
-  }
+    }
 }, false);
 
 
 
 
 function clearList() {
-    // var element = document.querySelector(".list-group-item");
-    // if (element.classList.contains("checked") === true) {
-    //     element.classList.toggle('empty');
-    // }
+        taskList1.tasks.pop(task); 
 
-    for (i = 0; i < taskList1.tasks.length; i++) {
-        task = taskList1.tasks[i]
-    }
+        console.log(taskList1.tasks);
+
 }
 
 
 
 var addItemToList = function(task) {
-    var node = document.createElement("LI"); 
-    node.className = 'list-group-item';                // Create a <li> node
-    var textnode = document.createTextNode(task);         // Create a text node
-    node.appendChild(textnode);                              // Append the text to <li>
-    document.getElementById("list1").appendChild(node);     // Append <li> to <ul> with id="myList"
+     // Append <li> to <ul> with id="myList"
 }
 
 
