5531008904bbc8f48f389ffb13505ed03df6336e
Joost Niessen <j.niessen@student.fontys.nl>
2019-12-19 10:37:13 +0100

    Added modules

--- Check&Hide/script.js
+++ Check&Hide/script.js
@@ -1,189 +1,253 @@
-var taskList = {
-    tasks: []
-}
 
 
-
-
-/***
- * Eventlistener on the + button in the DOM
- * sets the input field to ''
- * Contains:
- * addInputValuesToArray();
- * getInputValues(); // closure
- * upDateUI();
- */
-document.getElementById('addBtn').addEventListener('click', function() {
+var taskController = (function() {
     
-    addInputValuesToArray(getInputValues());
+})();
+
+var UIcontroller = (function() {
     
-    updateUI();
-     
-    console.log(taskList);
-
-    document.querySelector('#taskInput').value = '';
-    
-});
-
-
-
-/**
- * Eventlistener on the CLEAR LIST button in the DOM
- * clears array of checked items
- * clears the list of elements
- * Contains:
- * clearArray();
- * clearListElements();
- */
-document.getElementById('clearList').addEventListener('click', function() {
-    clearArray();
-    clearListElements();
-
-    console.log(taskList.tasks);
-    
-});
-
-
-
-/**
- * create array of all elements with tag <li> inside of #list1
- * loop through array to find 'checked' elements
- * if there are 'checked' elements, remove those
- */
-function clearListElements() {
-    var ul = document.getElementById("list1");
-    var items = ul.getElementsByTagName("li");
-    console.log(items.length);
-
-    
-    
-    for (var i = 0; i < items.length + 1; ++i) {
-        var el = document.querySelector('.checked');
-
-        if (el !== null){
-            el.remove(); 
-            
+    return {
+        getInput: function() {
+            return {        
+                value: document.querySelector('#taskInput').value
+            }
+        },
+        addInputToUI: function(taskName) {
+            taskList.tasks.push(taskName);
         }
-    }
-}
+        }
+    } 
 
 
 
-/***
- * sets the 'title' of the to do list
- * NOTE: this needs to be dynamic
- */
-document.querySelector('.header').textContent = 'Monday';
-
-
-/**
- * gets input from DOM and returns it
- */
-function getInputValues() {
-    var taskName = document.querySelector('#taskInput').value;
-    return taskName;
-}
-
-
-/***
- * requires value to push to array
- */
-function addInputValuesToArray(taskName) {
-    taskList.tasks.push(taskName);
-}
-
-/**
- * Loop: Gets all tasks from the array
- *       Creates a <li> element for every task and assigns Bootstrap-class to it
- * Appends listItems to List 
- */
-function updateUI() {
-    
-    for(let i = 0; i < taskList.tasks.length; i++) {
-
-        var task = taskList.tasks[i];
-
-        var list = document.createElement("LI"); 
-        list.className = 'list-group-item';                // Create a <li> node
-        var listItem = document.createTextNode(task);         // Create a text node
-    }
-
-    list.appendChild(listItem);                              // Append the text to <li>
-    document.getElementById("list1").appendChild(list);
-}
-
-
-/**
- * Selects UL, adds eventlistener on target
- * if target is LI toggle .checked, specified in the css
- */
-(function targetListelement() {
-    var list = document.querySelector('.list-group');
-    list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-    }
-}, false);
 })();
 
 
+var controller = (function(taskCtrl, UIctrl){
 
-/**
- * Selects list, adds eventlistener to click.
- * if checked is true, match content with index
- * use index to splice
- * ----- not working
- *   */
-function selectInArray() {
-    var list = document.querySelector('#list1');
+    var taskList = {
+        tasks: []
+    }
 
-    list.addEventListener('click', function(ev) {
-    let checked = list.querySelector('.checked');
-    if (checked !== null) {
+    var ctrlAddItem = function() {
+
+        // 1. get input
+        input = UIctrl.getInput();
+
+        // 2. add to array
+        UIctrl.addInputToUI(input);
+
+        // 3. add to UI 
         
 
-        var contentOfItem = checked.textContent;
 
-        var indexOfItem = taskList.tasks.indexOf(contentOfItem);
+        console.log('it works');
+    }
 
-        console.log(indexOfItem);
 
-        taskList.tasks.splice(indexOfItem, 1);
-        console.log(taskList.tasks);
+    var ctrlCheckItem = function() {
+        // 1. highlight clicked item
 
-        console.log(typeof(contentOfItem));
-        
+        // 2. store indexes in array
+
+        // 3. for each index of array splice()
+    };
+
+
+    var ctrlRemoveItem = function() {
+        //  1. check if clicked
+
 
     }
-}, false);
 
-}
+    document.getElementById('addBtn').addEventListener('click', ctrlAddItem);
 
-
-
-// array.prototype.indexof.call(parent.children, child)
+    document.getElementById('clearList').addEventListener('click', ctrlAddItem);
 
 
+})(taskController, UIcontroller);
 
-function clearArray() {
 
-    // check if element is checked
-    // if element is not null 
-    // remove element from array
-    // get split index
+
+// /***
+//  * Eventlistener on the + button in the DOM
+//  * sets the input field to ''
+//  * Contains:
+//  * addInputValuesToArray();
+//  * getInputValues(); // closure
+//  * upDateUI();
+//  */
+// document.getElementById('addBtn').addEventListener('click', function() {
     
-    // for(i = 0; i < taskList.tasks.length; i++) {
+//     addInputValuesToArray(getInputValues());
+    
+//     updateUI();
+     
+//     console.log(taskList);
+ 
+//     document.querySelector('#taskInput').value = '';
+    
+// });
 
-    //     var task = taskList.tasks[i];
 
-    //     var el = document.querySelector('.checked');
 
-    //     if (el !== null){
-    //         taskList.tasks.pop(task);      
-    //     }        // Create a text node
-    // }
+// /**
+//  * Eventlistener on the CLEAR LIST button in the DOM
+//  * clears array of checked items
+//  * clears the list of elements
+//  * Contains:
+//  * clearArray();
+//  * clearListElements();
+//  */
+// document.getElementById('clearList').addEventListener('click', function() {
+//     clearArray();
+//     clearListElements();
 
-}
+//     console.log(taskList.tasks);
+    
+// });
+
+
+
+// /**
+//  * create array of all elements with tag <li> inside of #list1
+//  * loop through array to find 'checked' elements
+//  * if there are 'checked' elements, remove those
+//  */
+// function clearListElements() {
+//     var ul = document.getElementById("list1");
+//     var items = ul.getElementsByTagName("li");
+//     console.log(items.length);
+
+    
+    
+//     for (var i = 0; i < items.length + 1; ++i) {
+//         var el = document.querySelector('.checked');
+
+//         if (el !== null){
+//             el.remove(); 
+            
+//         }
+//     }
+// }
+
+
+
+// /***
+//  * sets the 'title' of the to do list
+//  * NOTE: this needs to be dynamic
+//  */
+// document.querySelector('.header').textContent = 'Monday';
+
+
+// /**
+//  * gets input from DOM and returns it
+//  */
+// function getInputValues() {
+//     var taskName = document.querySelector('#taskInput').value;
+//     return taskName;
+// }
+
+
+// /***
+//  * requires value to push to array
+//  */
+// function addInputValuesToArray(taskName) {
+//     taskList.tasks.push(taskName);
+// }
+
+// /**
+//  * Loop: Gets all tasks from the array
+//  *       Creates a <li> element for every task and assigns Bootstrap-class to it
+//  * Appends listItems to List 
+//  */
+// function updateUI() {
+    
+//     for(let i = 0; i < taskList.tasks.length; i++) {
+
+//         var task = taskList.tasks[i];
+
+//         var list = document.createElement("LI"); 
+//         list.className = 'list-group-item';                // Create a <li> node
+//         var listItem = document.createTextNode(task);         // Create a text node
+//     }
+
+//     list.appendChild(listItem);                              // Append the text to <li>
+//     document.getElementById("list1").appendChild(list);
+// }
+
+
+// /**
+//  * Selects UL, adds eventlistener on target
+//  * if target is LI toggle .checked, specified in the css
+//  */
+// (function targetListelement() {
+//     var list = document.querySelector('.list-group');
+//     list.addEventListener('click', function(ev) {
+//     if (ev.target.tagName === 'LI') {
+//     ev.target.classList.toggle('checked');
+//     }
+// }, false);
+// })();
+
+
+
+// /**
+//  * Selects list, adds eventlistener to click.
+//  * if checked is true, match content with index
+//  * use index to splice
+//  * ----- not working
+//  *   */
+// function selectInArray() {
+//     var list = document.querySelector('#list1');
+
+//     list.addEventListener('click', function(ev) {
+//     let checked = list.querySelector('.checked');
+//     if (checked !== null) {
+        
+
+//         var contentOfItem = checked.textContent;
+
+//         var indexOfItem = taskList.tasks.indexOf(contentOfItem);
+
+//         console.log(indexOfItem);
+
+//         taskList.tasks.splice(indexOfItem, 1);
+//         console.log(taskList.tasks);
+
+//         console.log(typeof(contentOfItem));
+        
+
+//     }
+// }, false);
+
+// }
+
+
+
+// // array.prototype.indexof.call(parent.children, child)
+
+
+
+// function clearArray() {
+
+//     // check if element is checked
+//     // if element is not null 
+//     // remove element from array
+//     // get split index
+    
+//     // for(i = 0; i < taskList.tasks.length; i++) {
+
+//     //     var task = taskList.tasks[i];
+
+//     //     var el = document.querySelector('.checked');
+
+//     //     if (el !== null){
+//     //         taskList.tasks.pop(task);      
+//     //     }        // Create a text node
+//     // }
+
+// }
 
 
 
